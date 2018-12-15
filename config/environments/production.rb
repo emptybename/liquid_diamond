607efc2170a80ee1fc965d7df28d413bee0bcaf8
Mayank Shivhare <mayankankit888@gmail.com>
2018-12-15 19:13:13 +0530

    Reset config.action_mailer.default_url_options  in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://murmuring-basin-41080.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'murmuring-basin-41080.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
 end
